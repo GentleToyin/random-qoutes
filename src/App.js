a44e708a31f89b659ca781a01472e7f5330478ab
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-10-15 10:37:17 +0100

    Changes name to quotes, 50 different quotes added, randomly generated  when you refresh the page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { GenerateQoutes } from './components/qoutes-generator'
+import { GenerateQoutes } from './components/qoutes-generator';
+
 
 class App extends Component {
   render() {
-
-    const Qoutes = ['Oyelola', 'Toyin', 'Aliu', 'Samuel', 'Olawore', 'Oladipupo', 'Oladayo', 'Olugbenga', 'Abimbola', 'Lawumi']
-
-    const generateIndex = () => {
-      let randIndex = Math.floor(Math.random() * 10)
-      return randIndex
-    }
     return (
       <div className="App">
-        <GenerateQoutes qoutes={Qoutes[generateIndex()]} />
+        <GenerateQoutes />
+        {/* <button onClick={this.GenerateQoutes}>Random Name</button> */}
       </div>
     );
   }
--- src/components/qoutes-generator.js
+++ src/components/qoutes-generator.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { Qoutes } from "./qoutes";
-export const GenerateQoutes = (props) => (
-    <div>
-        <h1>Here</h1>
-        <h1>{props.qoutes}</h1>
-    </div>
-);
+import './qoutes-generator.css';
+import { Qoutes } from './qoutes';
+import quotationLeft from '../img/quotation-marks-left.svg';
+import quotationRight from '../img/quotation-marks-right.svg'
+
+export const GenerateQoutes = () => {
+
+    let randIndex = Math.floor(Math.random() * (((49 - 1) + 1) + 1))
+    let newQoute = Qoutes[randIndex]
+    return (
+
+        <div>
+            <div>
+                <h1 className="header">For Everyday You're Alive, Live!!</h1>
+            </div>
+            <div>
+                <img id="left" src={quotationLeft} alt="left quotation" />
+                <h1 className="main-container">{newQoute}</h1>
+                <img id="right" src={quotationRight} alt="right quotation" />
+            </div>
+
+        </div>
+
+    )
+};
 
 
 export default GenerateQoutes;
